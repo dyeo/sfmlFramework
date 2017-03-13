01fe917b8e293f695a6a73af9ac8713ec08553bf
Dan Yeomans <danyeomans98@gmail.com>
2017-03-13 16:45:48 -0400

    Update class introspection

--- sfmlFramework/Object.hpp
+++ sfmlFramework/Object.hpp
@@ -2,6 +2,7 @@
 
 //
 
+#define xstr(s) str(s)
 #define str(s) #s
 
 //
@@ -11,20 +12,20 @@ typedef const char* NameType;
 //
 
 #define CLASS_INTROSPECTION(CLASS_NAME) \
-public: \
-static constexpr ::NameType className = str(CLASS_NAME); \
-virtual ::NameType name() const \
-{ \
-return className; \
-} \
-bool compare(const ::NameType &rhs) const \
-{ \
-return name() == rhs; \
-} \
-bool compare(const CLASS_NAME &rhs) const \
-{ \
-return name() == rhs.name(); \
-} \
-private: \
+	public: \
+	static constexpr ::NameType className = xstr(CLASS_NAME); \
+	virtual ::NameType name() const \
+	{ \
+	return className; \
+	} \
+	bool compare(const ::NameType &rhs) const \
+	{ \
+	return name() == rhs; \
+	} \
+	bool compare(const CLASS_NAME &rhs) const \
+	{ \
+	return name() == rhs.name(); \
+	} \
+	private:
 
 //
